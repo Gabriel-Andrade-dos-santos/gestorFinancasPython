9650f4b252ab03693d63af68a4b19840ab0f289b
Gabriel-Andrade-dos-santos <gandradedossantos80@gmail.com>
2025-08-13 14:42:05 -0300

    listar_transacoes

--- projeto/transacoes.py
+++ projeto/transacoes.py
@@ -41,3 +41,11 @@ def adicionar_transacoes():
 
     informacoes.append(transacao)
     salvar_transacoes(informacoes)
+
+def listar_transacoes():
+    if not informacoes:
+        print("Ainda não há informações aqui. ")
+        return
+    
+    for ordem, valor in enumerate(informacoes, 1):
+        print(f"{ordem}) - {valor["data"]} - {valor["tipo"]} - {valor["valor"]} - {valor["descrição"]} - {valor["categoria"]}")
